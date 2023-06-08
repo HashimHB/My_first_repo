f8059d92b8e28b8a37243554c9cf7fc1e7532c8a
Hashim HB <hashimbasha97@gmail.com>
2023-06-08 22:17:56 +0530

    Added product.java

---
+++ product.java
@@ -0,0 +1,9 @@
+public class product {
+        public static void main(String args[]){
+            int a = 5;
+            int b = 6;
+            int product = a*b;
+            System.out.println("The product of two numbers is:"+product);
+        }
+    }
+
