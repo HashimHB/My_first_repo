c68273e33352fd22d6c76b7b34218a811a0ab8b2
Hashim HB <hashimbasha97@gmail.com>
2023-06-08 22:14:47 +0530

    Added diff..java

---
+++ diff.java
@@ -0,0 +1,8 @@
+public class diff {
+        public static void main(String args[]){
+            int a = 5;
+            int b = 6;
+            int difference = a-b;
+            System.out.println("The difference of two numbers is:"+difference);
+        }
+    }
